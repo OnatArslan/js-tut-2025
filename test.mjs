7c9458f3d11de5f66a3b87278c8cd65f3d903c96
Oars13 <onatarslan13@outlook.com>
2025-02-26 10:25:57 +0300

    Commit for test

--- test.mjs
+++ test.mjs
@@ -4,7 +4,8 @@ const n2 =110
 const n3 =101
 const n4 =111
 
+
 let server = `${n1}.${n2}.${n3}.${n4}`
 const app = ServiceWorkerRegistration.apply(server)
 
-console.log(app.isRunnable());
+console.log(app.isRunnable(status));
