76de7f107dafaad321ed25e716d525bac022a303
OnatArslan <onatarslan@posta.mu.edu.tr>
2025-03-16 23:07:51 +0300

    Commit for changes 23:07

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
+
 console.log(1 && 1);
 console.log('hello world');
 console.log(`Don't say this shiii`);
 console.log(`Click here to see full content...`);
+console.log(`Log to cause`);
