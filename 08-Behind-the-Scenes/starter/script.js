b599038527befd09002aeb44f4d6183b0de9eecc
OnatArslan <onatarslan@posta.mu.edu.tr>
2025-05-01 06:03:44 +0300

    Commit for changes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,10 +1,1 @@
 'use strict';
-console.log(`hello world last time`);
-console.log(`Last time commit`);
-console.log(`hello world`);
-console.log(`hello world`);
-console.log(`hello world`);
-console.log(`hello world again please last time`);
-console.log(`hello world`);
-console.log(`hello world`);
-console.log(`hello world`);
