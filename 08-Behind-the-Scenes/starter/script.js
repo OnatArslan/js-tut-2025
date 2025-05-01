3baa664ea8b0ebc331c4adb303010d568448dea1
OnatArslan <onatarslan@posta.mu.edu.tr>
2025-05-01 06:06:55 +0300

    Commit for changes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,7 +1,9 @@
 'use strict';
 
+// This currentYear variable is also in global scope
 const currentYear = 2025;
 
+// This calcAge function is in global scope
 function calcAge(birthDate) {
   return currentYear - birthDate;
 }
