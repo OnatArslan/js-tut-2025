4375df9f8b636de08c1a80d61c5ccdb060dca56f
OnatArslan <onatarslan@posta.mu.edu.tr>
2025-05-06 06:07:19 +0300

    Commit for changes

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -4,9 +4,15 @@
 
 const bookings = [];
 
-const createBooking = function (flightNum, numPassangers, price) {
-  numPassangers = numPassangers ?? 0;
-  price = price ?? 99;
+const createBooking = function (
+  flightNum = `IDK000`,
+  numPassangers = 1,
+  price = 99 * numPassangers, // we also use other parameters in
+) {
+  // This is old way for default parameters
+  //   flightNum = flightNum ?? `IDK000`;
+  //   numPassangers = numPassangers ?? 1;
+  //   price = price ?? 99;
   // creating booking object with parameter data
   const booking = {
     flightNum,
