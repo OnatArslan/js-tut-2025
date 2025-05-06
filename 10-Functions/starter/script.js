8b291678a0c67475a97c2137a6dac86cae44416d
OnatArslan <onatarslan@posta.mu.edu.tr>
2025-05-06 06:02:17 +0300

    Commit for changes

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,2 +1,19 @@
 'use strict';
-console.log('Hello world');
+
+// Default parameters
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassangers, price) {
+  // creating booking object with parameter data
+  const booking = {
+    flightNum,
+    numPassangers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking(`LH123`);
+console.log(bookings);
