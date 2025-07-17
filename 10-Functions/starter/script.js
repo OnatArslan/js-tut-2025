f41bca83b3222806f7b42690cf44c81cff92f6a6
OnatArslan <onatarslan@posta.mu.edu.tr>
2025-07-17 05:59:19 +0300

    Add checkIn and newPassport functions to demonstrate argument passing and object reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -18,3 +18,33 @@ function createBooking(
 }
 
 createBooking(23, 12);
+
+// PASSING ARGUMENTS - PASSING BY VALUE(PRIMITIVES) VS. PASSING BY REFERANCE
+const flight = `LH234`;
+const onat = {
+  name: `Onat Arslan`,
+  passport: 2330000000,
+};
+
+function checkIn(flightNum, passanger) {
+  flightNum = `LH999`;
+  passanger.name = `Mr.` + passanger.name;
+
+  if (passanger.passport === 2330000000) {
+    console.log(`Check in`);
+  } else {
+    console.log(`Wrong passport`);
+  }
+}
+
+checkIn(flight, onat);
+console.log(flight); // This flight data does not change because this is a primitive
+
+console.log(onat); // Onat variable is changed because this is a referance of onat object
+
+function newPassport(person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+}
+
+newPassport(onat);
+checkIn(flight, onat);
