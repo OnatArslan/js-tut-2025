f7a3c22f213ab7c3dc1875a5396004473373fff5
OnatArslan <onatarslan@posta.mu.edu.tr>
2025-07-17 05:22:25 +0300

    Implement createBooking function with default parameters and log booking details

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,2 +1,16 @@
 'use strict';
-console.log(`hello world`);
+
+const bookings = [];
+
+// In here we are giving default parameters
+function createBooking(flightNum = 1111, numPassengers = 1, price = 100) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+}
+
+createBooking();
